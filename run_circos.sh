2cc7fa298f5ce2aec973fab2f2e857590e9d1a6a
Renato G. H. Bombardelli <42347048+renato-bombardelli@users.noreply.github.com>
2020-03-04 15:59:30 -0300

    Create run_circos.sh

---
+++ run_circos.sh
@@ -0,0 +1,3 @@
+/home/renato/Downloads/circos-0.69-9/bin/circos -conf ./circos.conf -debug_group summary,timer > run.out
+
+#<circos> -conf <config file> -debug_group summary,timer > <out_error_log file>
